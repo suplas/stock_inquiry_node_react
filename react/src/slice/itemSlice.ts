8c4fe8e04228993b3b10608d3199fe97623f502f
suplas <suplas@naver.com>
2023-10-11 18:04:14 +0900

    build: react information under construction

--- react/src/slice/itemSlice.ts
+++ react/src/slice/itemSlice.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-type fetchStockType = {
+interface fetchStockDataType {
   id: number;
   BAS_DD: number;
   ISU_NM: string;
@@ -9,15 +9,26 @@ type fetchStockType = {
   MKT_NM: string;
   TDD_OPNPRC: number;
   ACC_TRDVOL: number;
-};
+}
+
+interface fetchStockType {
+  data: fetchStockDataType[];
+  totalPage: number;
+  totalData: number;
+}
+
+interface stockDataProps {
+  page: number;
+  limit: number;
+}
 
 export const fetchStockData = createAsyncThunk(
   "stock/items",
-  async (page: number, thunkAPI) => {
+  async ({ page, limit }: stockDataProps, thunkAPI) => {
     const response = await axios.get(
-      `http://219.250.2.138:5018/krx/api/list/${page}`
+      `http://219.250.2.138:5018/krx/api/list/${page}/${limit}`
     );
-    const responeData = response.data.data;
+    const responeData = response.data;
     return responeData as fetchStockType;
   }
 );
@@ -25,32 +36,54 @@ export const fetchStockData = createAsyncThunk(
 export interface stateProps {
   item: fetchStockType;
   fetchStatus: string;
+  page: number;
 }
 
 const initialState: stateProps = {
   item: {
-    id: 0,
-    BAS_DD: 0,
-    ISU_NM: "",
-    MKTCAP: 0,
-    MKT_NM: "",
-    TDD_OPNPRC: 0,
-    ACC_TRDVOL: 0,
+    data: [
+      {
+        id: 0,
+        BAS_DD: 0,
+        ISU_NM: "",
+        MKTCAP: 0,
+        MKT_NM: "",
+        TDD_OPNPRC: 0,
+        ACC_TRDVOL: 0,
+      },
+    ],
+    totalPage: 0,
+    totalData: 0,
   },
   fetchStatus: "",
+  page: 1,
 };
 
 const itemSlice = createSlice({
   name: "stock",
   initialState,
-  reducers: {},
+  reducers: {
+    pageIncrement: (state) => {
+      if (state.item.totalPage > state.page) {
+        state.page += 1;
+      }
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchStockData.pending, (state) => {
         state.fetchStatus = "loading";
       })
       .addCase(fetchStockData.fulfilled, (state, { payload }) => {
-        state.item = payload;
+        if (state.page > 1) {
+          payload.data.map((item) => {
+            state.item.data.push(item);
+          });
+        } else {
+          state.item.data = payload.data;
+        }
+        state.item.totalData = payload.totalData;
+        state.item.totalPage = payload.totalPage;
         state.fetchStatus = "success";
       })
       .addCase(fetchStockData.rejected, (state, action) => {
@@ -60,6 +93,5 @@ const itemSlice = createSlice({
   },
 });
 
-//export const { } = itemSlice.actions;
-//export const items = (state => state.item);
+export const { pageIncrement } = itemSlice.actions;
 export default itemSlice.reducer;
